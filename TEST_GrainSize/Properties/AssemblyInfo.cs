57899fc561c0104868a447d7c2214513ba5e1447
Антон Шибаев <sao@plm-ural.ru>
2023-06-20 13:31:54 +0500

    tests added (with libs problems)

---
+++ TEST_GrainSize/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TEST_GrainSize")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TEST_GrainSize")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2aa761ed-f724-481e-b6df-7b0c8d4e6abd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TEST_GrainSize/TESTS_FilatovModel/TEST_Filatov_S355NL.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Text;
+using System;
+using GrainSize_lib;
+using System.Data;
+
+namespace TEST_GrainSize
+{
+    [TestClass]
+    public class TEST_Filatov_S355NL
+    {
+        [TestMethod]
+        public void Test_CalkGrainSize()
+        {
+            float q = -1;
+            int objNumber = 1;
+            float currentTime = 5000;
+            DataTable materialTable = new DataTable();
+            DataTable resultsTable = new DataTable();
+            DataTable chemTable = new DataTable();
+            
+            materialTable.Columns.Add(new DataColumn("Индекс", typeof(int)));
+            materialTable.Columns.Add(new DataColumn("Временная экспонента", typeof(float)));
+            materialTable.Columns.Add(new DataColumn("Предэкспоненциальный множитель А", typeof(float)));
+
+            resultsTable.Columns.Add(new DataColumn("Индекс", typeof(int)));
+            resultsTable.Columns.Add(new DataColumn("Температура", typeof(float)));
+            DataRow resultTableRow = resultsTable.NewRow();
+            resultTableRow["Индекс"] = objNumber;
+            resultTableRow["Температура"] = 800;
+            resultsTable.Rows.Add(resultTableRow);
+
+
+
+            DataRow materialTableRow = materialTable.NewRow();
+            materialTableRow["Индекс"] = objNumber;
+            materialTableRow["Временная экспонента"] = 0.211;
+            materialTableRow["Предэкспоненциальный множитель А"] = 76671;
+
+            materialTable.Rows.Add(materialTableRow);
+            chemTable.Columns.Add(new DataColumn("Элемент", typeof(string)));
+            chemTable.Columns.Add(new DataColumn("%", typeof(float)));
+            DataRow chemRow1 = chemTable.NewRow();
+            chemRow1["Элемент"] = "C";
+            chemRow1["%"] = "0,18";
+            chemTable.Rows.Add(chemRow1);
+
+            DataRow chemRow2 = chemTable.NewRow();
+            chemRow2["Элемент"] = "Si";
+            chemRow2["%"] = "0,5";
+            chemTable.Rows.Add(chemRow2);
+
+            DataRow chemRow3 = chemTable.NewRow();
+            chemRow3["Элемент"] = "Mn";
+            chemRow3["%"] = "1,5";
+            chemTable.Rows.Add(chemRow3);
+
+            DataRow chemRow4 = chemTable.NewRow();
+            chemRow4["Элемент"] = "Cr";
+            chemRow4["%"] = "0,3";
+            chemTable.Rows.Add(chemRow4);
+
+            DataRow chemRow5 = chemTable.NewRow();
+            chemRow5["Элемент"] = "Cu";
+            chemRow5["%"] = "0,55";
+            chemTable.Rows.Add(chemRow5);
+
+            DataRow chemRow6 = chemTable.NewRow();
+            chemRow6["Элемент"] = "Mo";
+            chemRow6["%"] = "0,1";
+            chemTable.Rows.Add(chemRow6);
+
+            DataRow chemRow8 = chemTable.NewRow();
+            chemRow8["Элемент"] = "Ni";
+            chemRow8["%"] = "0,5";
+            chemTable.Rows.Add(chemRow8);
+
+            DataRow chemRow9 = chemTable.NewRow();
+            chemRow9["Элемент"] = "V";
+            chemRow9["%"] = "0,12";
+            chemTable.Rows.Add(chemRow9);
+
+            DataRow chemRow10 = chemTable.NewRow();
+            chemRow10["Элемент"] = "W";
+            chemRow10["%"] = "0,00";
+            chemTable.Rows.Add(chemRow10);
+
+            DataRow chemRow11 = chemTable.NewRow();
+            chemRow11["Элемент"] = "Al";
+            chemRow11["%"] = "0,02";
+            chemTable.Rows.Add(chemRow11);
+
+            DataRow chemRow12 = chemTable.NewRow();
+            chemRow12["Элемент"] = "P";
+            chemRow12["%"] = "0,025";
+            chemTable.Rows.Add(chemRow12);
+
+            DataRow chemRow13 = chemTable.NewRow();
+            chemRow13["Элемент"] = "As";
+            chemRow13["%"] = "0,00";
+            chemTable.Rows.Add(chemRow13);
+
+            DataRow chemRow14 = chemTable.NewRow();
+            chemRow14["Элемент"] = "Ti";
+            chemRow14["%"] = "0,05";
+            chemTable.Rows.Add(chemRow14);
+            var result = new GrainSize_Calc_Filatov();
+            var results = result.CalcGrainSize(objNumber, q, currentTime, chemTable, materialTable, resultsTable);
+
+
+            Assert.AreEqual(Math.Round(results, 3), 0,349);
+        }
+    }
+}
